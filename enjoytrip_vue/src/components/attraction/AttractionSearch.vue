f89eebf154c7b2588cdeb49c2051fdf31159e6b9
유영서 <you2882@naver.com>
2023-05-18 15:43:56 +0900

    Add : AttractionSearch

---
+++ enjoytrip_vue/src/components/attraction/AttractionSearch.vue
@@ -0,0 +1,31 @@
+<template>
+      <b-container class="bv-example-row mt-3">
+    <attraction-search-item></attraction-search-item>
+    <attraction-map></attraction-map>
+    <attraction-list></attraction-list>
+  </b-container>
+</template>
+
+<script>
+import AttractionSearchItem from "@/components/attraction/item/AttractionSearchItem.vue";
+import AttractionMap from "@/components/attraction/AttractionMap.vue";
+import AttractionList from "./AttractionList.vue";
+
+export default {
+    name: 'AttractionSearch',
+    components: {
+        AttractionSearchItem,
+        AttractionMap,
+        AttractionList,
+    },
+    data() {
+    return {
+            message: '',
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
