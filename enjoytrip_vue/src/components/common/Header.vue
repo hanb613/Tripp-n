071b18b212a30cbf28b3d1dcca46d299f660709d
skh1615 <skh1615@gmail.com>
2023-05-19 16:40:08 +0900

    Fix: 헤더 유저메뉴 드롭다운

--- enjoytrip_vue/src/components/common/Header.vue
+++ enjoytrip_vue/src/components/common/Header.vue
@@ -13,7 +13,7 @@
       <b-nav id="navbar" class="navbar">
         <b-nav-item href="#">
           <router-link :to="{ name: 'attraction' }" class="link">
-            <b-icon icon="map" font-scale="2"  class="m-1"></b-icon>
+            <b-icon icon="map" font-scale="2" class="m-1"></b-icon>
             관광지 조회
           </router-link>
           <router-link :to="{ name: 'board' }" class="m-2 link">
@@ -21,58 +21,61 @@
             공지사항
           </router-link>
           <router-link :to="{ name: 'board' }" class="m-2 link">
-            <b-icon icon="clipboard" font-scale="2"  class="m-1"></b-icon>
+            <b-icon icon="clipboard" font-scale="2" class="m-1"></b-icon>
             게시판
           </router-link>
           <router-link :to="{ name: 'house' }" class="m-2 link">
-            <b-icon icon="bookmark-star" font-scale="2"  class="m-1"></b-icon>
+            <b-icon icon="bookmark-star" font-scale="2" class="m-1"></b-icon>
             관심 목록
           </router-link>
         </b-nav-item>
       </b-nav>
       <!-- after login -->
-      <b-navbar-nav class="ml-auto" v-if="userInfo">
-        <b-nav-item-dropdown right>
-          <template #button-content>
-            <b-icon icon="people" font-scale="2"></b-icon>
-          </template>
-          <b-dropdown-item href="#">
-            <router-link
-            :to="{ name: 'UserMyPage' }"
-            class="link align-self-center"
-            >
-            <b-icon icon="person-circle"></b-icon>&nbsp; My Page</router-link
+      <b-navbar-nav class="ml-auto navbar" v-if="userInfo">
+        <b-nav-item right>
+          <b-dropdown
+            id="dropdown-1"
+            :text="userInfo.name"
+            class="m-md-2"
+            variant="outline-light"
           >
-          </b-dropdown-item>
-          <b-dropdown-item @click.prevent="onClickLogout">
-              <b-icon icon="key" ></b-icon>&nbsp;  로그아웃
-          </b-dropdown-item>
-        </b-nav-item-dropdown>
+            <b-dropdown-item>
+              <router-link :to="{ name: 'UserMyPage' }" class="link">
+                마이페이지
+              </router-link>
+            </b-dropdown-item>
+            <b-dropdown-item class="link" @click.prevent="onClickLogout"
+              >로그아웃</b-dropdown-item
+            >
+          </b-dropdown>
+        </b-nav-item>
       </b-navbar-nav>
       <!-- before login -->
-      <b-navbar-nav class="ml-auto" v-else>
-        <b-nav-item-dropdown right>
-          <template #button-content>
-            <b-icon icon="people" font-scale="2"></b-icon>
-          </template>
-          <b-dropdown-item href="#">
-            <router-link :to="{ name: 'UserRegister' }" class="link">
-              <b-icon icon="person-circle"></b-icon>&nbsp; 회원가입
-            </router-link>
-          </b-dropdown-item>
-          <b-dropdown-item href="#">
-            <router-link :to="{ name: 'UserLogin' }" class="link">
-              <b-icon icon="key"></b-icon>&nbsp;&nbsp; 로그인
-            </router-link>
-          </b-dropdown-item>
-        </b-nav-item-dropdown>
+      <b-navbar-nav class="ml-auto navbar" v-else>
+        <b-nav-item right>
+          <b-nav-item-dropdown>
+            <template #button-content>
+              <b-icon icon="people" font-scale="2"></b-icon>
+            </template>
+            <b-dropdown-item>
+              <router-link :to="{ name: 'UserRegister' }" class="link">
+                <b-icon icon="person-circle"></b-icon>&nbsp; 회원가입
+              </router-link>
+            </b-dropdown-item>
+            <b-dropdown-item>
+              <router-link :to="{ name: 'UserLogin' }" class="link">
+                <b-icon icon="key"></b-icon>&nbsp;&nbsp; 로그인
+              </router-link>
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-nav-item>
       </b-navbar-nav>
     </div>
   </header>
 </template>
   
 <script>
-  import { mapState, mapGetters, mapActions } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 const memberStore = "memberStore";
 
@@ -90,8 +93,8 @@ export default {
     // ...mapMutations(memberStore, ["SET_IS_LOGIN", "SET_USER_INFO"]),
     onClickLogout() {
       this.userLogout(this.userInfo.id);
-      alert("Logout! 🙌")
-      if (this.$route.path != "/") this.$router.push('/');
+      alert("Logout! 🙌");
+      if (this.$route.path != "/") this.$router.push("/");
     },
   },
 };
@@ -101,7 +104,6 @@ export default {
 .navbar .nav-item {
   list-style: none;
 }
-
 
 @import "../../assets/vendor/bootstrap/css/bootstrap.min.css";
 @import "../../assets/vendor/bootstrap-icons/bootstrap-icons.css";
