bec59bce49dcac0aa8abef4bfab92dcdf4dd31d0
skh1615 <skh1615@gmail.com>
2023-05-19 17:10:08 +0900

    Fix: > 지움

--- enjoytrip_vue/src/components/attraction/AttractionView.vue
+++ enjoytrip_vue/src/components/attraction/AttractionView.vue
@@ -1,6 +1,6 @@
 <template>
-  <b-container class="bv-example-row mt-3" >
-    <b-row class="mb-1" >
+  <b-container class="bv-example-row mt-3">
+    <b-row class="mb-1">
       <b-col id="container">
         <b-card
           :img-src="attraction.firstImage"
@@ -8,28 +8,50 @@
           img-alt="관광지 사진"
         >
           <b-card-text>
-            <h2 id="font"><b>{{ attraction.title }}</b></h2><br />
-            <span id="font"><b>우편번호</b><br> {{ attraction.zipcode }}</span><br /><br />
-            <span id="font"><b>주소</b><br>{{ attraction.addr1 }}</span><br /><br />
-            
-            <span id="font" v-if="attraction.tel"><b>전화번호</b><br>{{ attraction.tel }}<br /><br /></span>
-            
-            <span id="font"><b>상세정보</b><br> {{ attraction.overview }}</span>
+            <h2 id="font">
+              <b>{{ attraction.title }}</b>
+            </h2>
+            <br />
+            <span id="font"
+              ><b>우편번호</b><br />
+              {{ attraction.zipcode }}</span
+            ><br /><br />
+            <span id="font"><b>주소</b><br />{{ attraction.addr1 }}</span
+            ><br /><br />
+
+            <span id="font" v-if="attraction.tel"
+              ><b>전화번호</b><br />{{ attraction.tel }}<br /><br
+            /></span>
+
+            <span id="font"
+              ><b>상세정보</b><br />
+              {{ attraction.overview }}</span
+            >
           </b-card-text>
-            <b-button pill class="float-right" variant="outline-success" @click="moveList()">
-              <b-icon icon="list" font-scale="1" style="margin-right:5px"></b-icon>
-                <span id="font">목록</span>
-            </b-button>
-            <b-button pill class="me-2 float-right" variant="primary">
-                <b-icon icon="suit-heart" font-scale="1" style="margin-right:5px"></b-icon>
-                <span id="font">좋아요</span>
-            </b-button >
+          <b-button
+            pill
+            class="float-right"
+            variant="outline-success"
+            @click="moveList()"
+          >
+            <b-icon
+              icon="list"
+              font-scale="1"
+              style="margin-right: 5px"
+            ></b-icon>
+            <span id="font">목록</span>
+          </b-button>
+          <b-button pill class="me-2 float-right" variant="primary">
+            <b-icon
+              icon="suit-heart"
+              font-scale="1"
+              style="margin-right: 5px"
+            ></b-icon>
+            <span id="font">좋아요</span>
+          </b-button>
         </b-card>
       </b-col>
-      
-        >
     </b-row>
-    
   </b-container>
 </template>
 
@@ -58,29 +80,27 @@ export default {
   },
   methods: {
     moveList() {
-      this.$router.push({name :"AttractionSearch"})
-    }
+      this.$router.push({ name: "AttractionSearch" });
+    },
   },
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
-
-#container{
+#container {
   width: 100%;
   display: flex;
   justify-content: center;
   align-content: center;
 }
 
-#font{
-  font-family: 'Noto Sans KR', sans-serif;
+#font {
+  font-family: "Noto Sans KR", sans-serif;
 }
 
-.img{
-  width:60%;
-
+.img {
+  width: 60%;
 }
 </style>
